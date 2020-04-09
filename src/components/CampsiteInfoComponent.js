d279b62051c8806c23d8629b32405bff60eab147
B Herrington <bjherrington7@gmail.com>
2020-04-09 11:34:07 -0500

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -21,7 +21,7 @@ function RenderCampsite({campsite}) {
     );
   }
 
- function RenderComments({comments, addComment, campsiteId}) {
+ function RenderComments({comments, postComment, campsiteId}) {
     if (comments) {
       return (
         <div className="col-md-5 m-1">
@@ -34,7 +34,7 @@ function RenderCampsite({campsite}) {
               </div>
             );
           })}
-          <CommentForm campsiteId={campsiteId} addComment={addComment} />
+          <CommentForm campsiteId={campsiteId} postComment={postComment} />
         </div>
       );
     }
@@ -58,7 +58,7 @@ function RenderCampsite({campsite}) {
      }
      handleSubmit(values) {
        this.toggleModal();
-       this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+       this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
      }
     render() {
       return (
@@ -160,7 +160,7 @@ function RenderCampsite({campsite}) {
               <RenderCampsite campsite={props.campsite} />
               <RenderComments
                 comments={props.comments}
-                addComment={props.addComment}
+                postComment={props.postComment}
                 campsiteId={props.campsite.id}
               />
           </div>
